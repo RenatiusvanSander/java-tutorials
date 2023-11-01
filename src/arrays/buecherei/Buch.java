c842a7668859046de0e9001be72f6679582dded9
RenatiusvanSander <remad@web.de>
2023-11-01 17:46:23 +0100

    master: the library without library but has books

---
+++ src/arrays/buecherei/Buch.java
@@ -0,0 +1,81 @@
+package arrays.buecherei;
+
+import java.util.Objects;
+
+public class Buch {
+
+	private String autor;
+	
+	private String titel;
+	
+	private String jahr;
+	
+	private String ISBN;
+
+	public Buch() {
+		super();
+	}
+
+	public Buch(String autor, String titel, String jahr, String iSBN) {
+		super();
+		this.autor = autor;
+		this.titel = titel;
+		this.jahr = jahr;
+		ISBN = iSBN;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getTitel() {
+		return titel;
+	}
+
+	public void setTitel(String titel) {
+		this.titel = titel;
+	}
+
+	public String getJahr() {
+		return jahr;
+	}
+
+	public void setJahr(String jahr) {
+		this.jahr = jahr;
+	}
+
+	public String getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(ISBN, autor, jahr, titel);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Buch other = (Buch) obj;
+		return Objects.equals(ISBN, other.ISBN) && Objects.equals(autor, other.autor)
+				&& Objects.equals(jahr, other.jahr) && Objects.equals(titel, other.titel);
+	}
+
+	@Override
+	public String toString() {
+		return "Buch [autor=" + autor + ", titel=" + titel + ", jahr=" + jahr + ", ISBN=" + ISBN + "]";
+	}
+}
---
+++ src/arrays/buecherei/readme.md
@@ -0,0 +1,19 @@
+Wir erstellen eine Buecher mit Autoren, Jahr, Auflage und Titel.
+
+Wir erzeugen die Buecherei-Klasse, welche Buecher verwalten soll. Beim Start
+ des Programms werden folgende Optionen dem Buechreibesucher angezeigt:
+ a) Buch bestellen
+ b) Buecher eines Autorens ermitteln
+ c) Titelsuchen
+ d) Buch anlegen
+ e) Buch per ISBN suchen.
+ 
+ Nach Ausführung einer Option kehrt das Programm zur Optionsanzeige zurück und werden solang angezeigt, bis Taste "q" für beenden gdrückt wird.
+ Groß- und Kleinschreibung spielt bei q keine Rolle.
+ 
+ 10 Bücher sollen bereits in der Bücherei vorhanden sein.
+ Mit Buch anlegege kommt ein Buch hinzu, also ein Platz mehr im Feldlinienartigen Buechereiregal, dass für uns ein Array ist.
+ Ein weiteres Array kommt für die Buch Bestellung hinzu..
+ Buecher eines Autoren werden ermittelt, indem der gesuchte Autor im Buch gefunden wird und ermittelten Bücher hinzugefügt wird..
+ Bie Titelsuche werden alle Bücher nach genau diesem Titel durchsucht.
+ Bei Buch per ISBN suchen werden alle Bücher nach genau dieser ISBN durchsucht und nur der Treffer angezeigt.
