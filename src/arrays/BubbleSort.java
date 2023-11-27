872b3510b4fe7b2a6cdce632e6a4b4956fbe804b
Remy Meier <remad@web.de>
2023-11-27 18:32:08 +0100

    master: BubbleSort Aufgabe

---
+++ src/arrays/BubbleSort.java
@@ -0,0 +1,10 @@
+package arrays;
+
+public class BubbleSort {
+
+	private int[] array;
+	
+	public static void main(String[] args ) {
+		System.out.println("1. Programmieren sie BubbleSort.");
+	}
+}
