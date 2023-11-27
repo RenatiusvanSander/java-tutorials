1717cac82df9be4447d0fbcf0ddacd885a7ef47b
Remy Meier <remad@web.de>
2023-11-27 18:13:40 +0100

    master: kuerzester Weg in das Kino

---
+++ src/arrays/kurzerweginskino/Kinoweg.java
@@ -0,0 +1,12 @@
+package arrays.kurzerweginskino;
+
+import java.awt.Point;
+
+public class Kinoweg {
+
+	private Point[] points = { new Point(10, 20), new Point(12, 2), new Point(44, 4) };
+	
+	private static void main(String[] args) {
+		System.out.println("1. finde den kuerzesten Weg zum Kino heraus.");
+	}	
+}
