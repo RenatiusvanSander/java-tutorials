aee305d3a3470bbf48acbc868860e4e7029a200e
Remy Meier <remad@web.de>
2023-11-27 18:06:54 +0100

    master: reverse array task

---
+++ src/arrays/reversearray/ReverseArray.java
@@ -0,0 +1,14 @@
+package arrays.reversearray;
+
+public class ReverseArray {
+
+	private static int[] zahlenFelder = new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+
+	public static void main(String[] args) {
+		System.out.println("1. Wir wollen ein Array umkehren und ausgeben.");
+		System.out.println("2. Wir wollen ein Benutzer-eingebene Felder umkehren und ausgeben.");
+	}
+	
+	private static void reverse(int[] numbers) {
+	}
+}
